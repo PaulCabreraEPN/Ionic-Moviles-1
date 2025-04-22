f595fc45eb582182f048f84afa1510c5d43ea9c5
PaulCabreaEPN <paul.cabrera@epn.edu.ec>
2025-04-22 10:12:44 -0500

    Second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonToast,
+  IonCard,
+  IonChip,
+  IonButton,
+  IonRadio,
+  IonDatetime,
+  IonRadioGroup,
+  IonCheckbox, 
+  IonPicker, 
+  IonPickerColumn, 
+  IonPickerColumnOption,
+  IonToggle,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonPicker, IonToggle, IonPickerColumn, IonPickerColumnOption, CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonToast, IonCard, IonChip, IonButton, IonRadio, IonRadioGroup, IonCheckbox, IonDatetime],
 })
 export class HomePage {
   constructor() {}
